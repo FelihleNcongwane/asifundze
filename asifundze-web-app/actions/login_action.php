3a6352be1381490fa7103bb5d0e17682ccf3a028
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-03-10 08:17:17 +0000

    Login working

--- asifundze-web-app/actions/login_action.php
+++ asifundze-web-app/actions/login_action.php
@@ -1,3 +1,27 @@
 <?php
 
+session_start();
 include '../config/connection.php';
+global $conn;
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM Users WHERE email = '$email'";
+
+    if($result = mysqli_query($conn, $sql)){
+        $row = mysqli_fetch_assoc($result);
+        $hash = $row['password'];
+
+        if(password_verify($password, $hash)){
+            $_SESSION['uid'] = $row['id'];
+            header('Location: ../personal/dashboard.html');
+        }else{
+            $_SESSION['error'] = "Invalid email or password";
+            header('Location: ../user/login.html');
+        }
+        exit();
+    }
+
+}
