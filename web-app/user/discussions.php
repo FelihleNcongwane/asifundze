656fdfed4f6ad4df57d3b13801699dc10edff62f
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-03-16 01:05:11 +0000

    move image folder to assets

--- web-app/user/discussions.php
+++ web-app/user/discussions.php
@@ -5,13 +5,13 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title> Discussion Page</title>
       <link rel="stylesheet" href="../css/style.css">
-      <link rel="stylesheet" href="../css/discussion_style.css">
+      <link rel="stylesheet" href="../css/discussion.css">
    </head>
    <body>
    <main class="cd__main">
        <div class="container">
            <div class="response-group">
-               <header>
+               <header id="heading">
                    <h2>
                        <strong>Apple CEO</strong>
                        <i class="fa fa-angle-right"></i>
@@ -60,50 +60,6 @@
                            <div class="post__avatar"></div>
                            <h3 class="post__author">Emmanuel Okine</h3>
                            <h4 class="post__timestamp">every second</h4>
-                           <p class="post__body">Rough set theories are utilized in class-specific feature selection to improve the classification performance of continuous data while handling data uncertainty. However, most of those approaches are converting continuous data into discrete or fuzzy data before applying rough set theories. These data conversions can reduce or change the meaning of data, as well as introduce unnecessary complexity to the feature domain. Therefore, in this study, we use neighborhood rough sets in class-specific feature selection to improve the classification performance without data conversions. As the standard classification algorithms are capable of handling a single feature set, we propose a novel classification algorithm based on the K-Nearest Neighbour algorithm to use class-specific feature subsets. Experimental evaluations prove that the proposed approach outperforms most of the baselines, and the selected feature sets are more effective than using the full feature sets in classification. The approach highly reduces the number of selected features and hence, can be used for effective data analysis of continuous data with high performance.</p>
-                           <div class="post__actions">
-                               <div class="button button--approve">
-                                   <i class="fa fa-thumbs-o-up"></i><i class="fa fa-thumbs-up solid"></i>
-                               </div>
-                               <div class="button button--deny">
-                                   <i class="fa fa-thumbs-o-down"></i><i class="fa fa-thumbs-down solid"></i>
-                               </div>
-                               <div class="button button--fill comment-trigger">
-                                   <span>Comment...</span>
-                               </div>
-                               <div class="button button--flag">
-                                   <i class="fa fa-comment-o"></i><i class="fa fa-comment solid"></i>2
-                               </div>
-                               <div class="post__comments">
-                                   <div class="comment-group">
-                                       <div class="post">
-                                           <div class="post__avatar comment__avatar"></div>
-                                           <h3 class="post__author">Emmanuel Okine</h3>
-                                           <h4 class="post__timestamp">every second</h4>
-                                           <p class="post__body">I am the coder!</p>
-                                       </div>
-                                       <div class="post">
-                                           <div class="post__avatar comment__avatar"></div>
-                                           <h3 class="post__author">Emmanuel Okine</h3>
-                                           <h4 class="post__timestamp">every second</h4>
-                                           <p class="post__body">School is hard for people but not for me. Because I am not people.</p>
-                                       </div>
-                                   </div>
-                                   <div class="comment-form">
-                                       <div class="comment-form__avatar"></div>
-                                       <textarea></textarea>
-                                       <div class="comment-form__actions">
-                                           <div class="button button--light cancel">Cancel</div>
-                                           <div class="button button--confirm">Comment</div>
-                                       </div>
-                                   </div>
-                               </div>
-                           </div>
-                       </div>
-                       <div class="post">
-                           <div class="post__avatar"></div>
-                           <h3 class="post__author">Emmanuel Okine</h3>
-                           <h4 class="post__timestamp">every second</h4>
                            <p class="post__body">I am the coder!</p>
                            <div class="post__actions">
                                <div class="button button--approve">
@@ -143,55 +99,11 @@
                                </div>
                            </div>
                        </div>
-                       <div class="post">
-                           <div class="post__avatar"></div>
-                           <h3 class="post__author">Emmanuel Okine</h3>
-                           <h4 class="post__timestamp">every second</h4>
-                           <p class="post__body">I am the coder!</p>
-                           <div class="post__actions">
-                               <div class="button button--approve">
-                                   <i class="fa fa-thumbs-o-up"></i><i class="fa fa-thumbs-up solid"></i>
-                               </div>
-                               <div class="button button--deny">
-                                   <i class="fa fa-thumbs-o-down"></i><i class="fa fa-thumbs-down solid"></i>
-                               </div>
-                               <div class="button button--fill comment-trigger">
-                                   <span>Comment...</span>
-                               </div>
-                               <div class="button button--flag">
-                                   <i class="fa fa-comment-o"></i><i class="fa fa-comment solid"></i>2
-                               </div>
-                               <div class="post__comments">
-                                   <div class="comment-group">
-                                       <div class="post">
-                                           <div class="post__avatar comment__avatar"></div>
-                                           <h3 class="post__author">Emmanuel Okine</h3>
-                                           <h4 class="post__timestamp">every second</h4>
-                                           <p class="post__body">I am the coder!</p>
-                                       </div>
-                                       <div class="post">
-                                           <div class="post__avatar comment__avatar"></div>
-                                           <h3 class="post__author">Emmanuel Okine</h3>
-                                           <h4 class="post__timestamp">every second</h4>
-                                           <p class="post__body">I am the coder!</p>
-                                       </div>
-                                   </div>
-                                   <div class="comment-form">
-                                       <div class="comment-form__avatar"></div>
-                                       <textarea></textarea>
-                                       <div class="comment-form__actions">
-                                           <div class="button button--light cancel">Cancel</div>
-                                           <div class="button button--confirm">Comment</div>
-                                       </div>
-                                   </div>
-                               </div>
-                           </div>
-                       </div>
                    </div>
                </div>
            </div>
        </div>
    </main>
-   <script src="./js/script.js"></script>
+   <script src="../js/script.js"></script>
    </body>
 </html>
