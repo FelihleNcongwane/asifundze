7a54611ab9153d425b227d9fdce93492d99dc99f
Victor  Quagraine <Victorquagraine9@gmail.com>
2024-03-17 23:41:05 +0000

    fix comment

--- web-app/user/discussions.php
+++ web-app/user/discussions.php
@@ -19,49 +19,16 @@
        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
    </head>
    <body>
-<!--   <nav class="nav">-->
-<!--       <div class="nav-logo">-->
-<!--           <p>asifundze</p>-->
-<!--       </div>-->
-<!--       <div class="nav-menu" id="navMenu">-->
-<!--           <ul>-->
-<!--               <li><a href="../index.html" class="link">Home</a></li>-->
-<!--               <li><a href="../view/about.html" class="link">About Us</a></li>-->
-<!--               <li><a href="../view/contact.html" class="link">Contact Us</a></li>-->
-<!--           </ul>-->
-<!--       </div>-->
-<!--       <div class="nav-button">-->
-<!--           <button class="btn white-btn" id="loginBtn" onclick="login()">Sign In</button>-->
-<!--           <button class="btn" id="registerBtn" onclick="register()">Sign Up</button>-->
-<!--       </div>-->
-<!--       <div class="nav-menu-btn">-->
-<!--           <i class="bx bx-menu" onclick="myMenuFunction()"></i>-->
-<!--       </div>-->
-<!--   </nav>-->
-
    <main class="cd__main">
        <div class="container">
            <div class="response-group">
                <header id="heading">
                    <h2>
                        <a href="../html/1intro.php" style="text-decoration: none; color:white;"><strong>HTML</strong></a>
-<!--                       <i class="fa fa-angle-right"></i>-->
-<!--                       <span class="header-dropdown-trigger">HTML Tags</span>-->
+
                    </h2>
                </header>
-<!--               <div class="header-dropdown">-->
-<!--                   <div class="panel">-->
-<!--                       <ul>-->
-<!--                           <li>-->
-<!--                               <h2>Activity Title</h2>-->
-<!--                               <span>2/4</span>-->
-<!--                               <div class="progress">-->
-<!--                                   <div class="progress__complete"></div>-->
-<!--                               </div>-->
-<!--                           </li>-->
-<!--                       </ul>-->
-<!--                   </div>-->
-<!--               </div>-->
+
                <div class="response">
                    <div class="response__number" id="comment_counter"></div>
                    <h1 class="response__title">Discussion Page</h1>
@@ -88,57 +55,6 @@
          });
 
    </script>
-
-
-<script>
-$(document).ready(function(){
- 
- $('#post_id').on('submit', function(event){
-  event.preventDefault();
-  var form_data = $(this).serialize();
-  $.ajax({
-   url:"../functions/get_comment_count.php",
-   method:"POST",
-   data:form_data,
-   dataType:"JSON",
-   success:function(data)
-   {
-    if(data.error != '')
-    {
-     $('#post_id')[0].reset();
-     $('#post').html(data.error);
-     $('#comment_id').val('0');
-     load_comment();
-    }
-   }
-  })
- });
-
- load_comment();
-
- function load_comment()
- {
-  $.ajax({
-   url:"../functions/get_comment_count.php",
-   method:"POST",
-   success:function(data)
-   {
-    $('#posts').html(data);
-   }
-  })
- }
-
- $(document).on('click', '.reply', function(){
-  var comment_id = $(this).attr("id");
-  $('#comment_id').val(comment_id);
-  $('#comment_name').focus();
- });
- 
-});
-</script>
-
-
-
    <script src="../js/script.js"></script>
    </body>
 </html>
