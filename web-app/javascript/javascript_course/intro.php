318a611afd2c43e75c0dd1ba69e61c4a44489c3c
JanetBoye <123462261+JanetBoye@users.noreply.github.com>
2024-03-17 21:39:15 +0000

    Adding intro page for Javascript course

---
+++ web-app/javascript/javascript_course/intro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>ASIFUNDZE-JavaScript</title>
+	<link rel="stylesheet" href="javascriptcourse.css">
+</head>
+<body>
+	<header id="head" class="secondary">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8">
+					<h1>JavaScript</h1>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-9 col-md-push-2" style="margin-bottom:10px;">
+					<h2>JavaScript Plays a huge role in front end development and is very important in creating dynamic and interactive websites </h2>
+					<a href="Chapter1.php" class="btn">Start Learning!</a>
+					</form>
+</body>
+</html>
